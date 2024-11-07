b4ce05db2281d283b6ad4b62cdcc89009a551aab
vini5 <viniglowacki@gmail.com>
2024-11-07 09:15:56 -0300

    change order foo() in tests

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -1,11 +1,10 @@
 import context
-
 from src.agent import func
-
-
-def test_func():
-    assert func() == 1
 
 
 # Para o ruff não remover o import do context
 context.foo()
+
+
+def test_func():
+    assert func() == 1
